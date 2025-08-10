4994ca895c17048619c1df2e680952586db5f536
ManishBarath <barath04.11.05@gmail.com>
2025-08-10 20:46:29 +0530

    added env

--- app/utils/exports.ts
+++ app/utils/exports.ts
@@ -1,3 +1,3 @@
 
 export const RAZORPAY_KEY_ID = process.env.RAZORPAY_KEY_ID ;
-export const API_BASE_URL = process.env.API_BASE_URL ;
+export const API_BASE_URL = process.env.VITE_API_BASE_URL || 'https://hnm2-be.vercel.app';
