cf50bb8461bc9e1fd7fa2473c90d83a4dd198b47
ManishBarath <barath04.11.05@gmail.com>
2025-08-10 20:36:54 +0530

    added env

--- app/routes/registration.tsx
+++ app/routes/registration.tsx
@@ -800,7 +800,7 @@ type PurchasedPlan = {
   purchaseDate: string;
   paymentId: string;
 };
-const API_BASE_URL = process.env.API_BASE_URL ;
+const API_BASE_URL = process.env.VITE_API_BASE_URL ;
 const passes: Record<string, Pass[]> = {
   "Day 1": [
     {
